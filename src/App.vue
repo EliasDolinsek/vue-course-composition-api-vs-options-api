ad0450fe9b4faeab55fe1484598c0488900c1140
eld <elias.dolinsek@gmail.com>
2022-09-13 20:35:27 +0200

    Finished TheOptionsApiStopwatch

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <script setup>
-import TheCompositionApiStopwatch from './components/TheCompositionApiStopwatch.vue';
+import TheCompositionApiStopwatch from "./components/TheCompositionApiStopwatch.vue";
+import TheOptionsApiStopwatch from "./components/TheOptionsApiStopwatch.vue";
 </script>
 
 <template>
   <main>
     <div>
-      <TheCompositionApiStopwatch class="stopwatch"/>
+      <TheCompositionApiStopwatch class="stopwatch" />
     </div>
-    <p>Test</p>
+    <div class="separator" />
+    <div>
+      <TheOptionsApiStopwatch class="stopwatch" />
+    </div>
   </main>
 </template>
 
 <style scoped>
 main {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1px 1fr;
 }
 
 .stopwatch {
   margin-left: 16px;
   margin-right: 16px;
+}
+
+.separator {
+  min-height: 100vh;
+  min-width: 1px;
+  background: rgba(0, 0, 0, 0.12);
 }
 </style>
