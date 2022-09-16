36309711abdf682751cf3ff0a9d2a2e5a18c8794
eld <elias.dolinsek@gmail.com>
2022-09-16 20:20:53 +0200

    Updated App component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <script setup>
-import TheCompositionApiStopwatch from './components/TheCompositionApiStopwatch.vue';
+import TheCompositionApiStopwatch from "./components/TheCompositionApiStopwatch.vue";
 </script>
 
 <template>
   <main>
     <div>
-      <TheCompositionApiStopwatch class="stopwatch"/>
+      <TheCompositionApiStopwatch class="stopwatch" />
     </div>
-    <p>Options API</p>
+    <div class="separator" />
+    <div>Options API</div>
   </main>
 </template>
 
 <style scoped>
 main {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1px 1fr;
 }
 
 .stopwatch {
   margin-left: 16px;
   margin-right: 16px;
+}
+
+.separator {
+  min-height: 100vh;
+  min-width: 1px;
+  background: rgba(0, 0, 0, 0.12);
 }
 </style>
